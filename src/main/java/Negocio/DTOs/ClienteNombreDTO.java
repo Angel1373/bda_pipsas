2fd82cef4b4c7cd51a948215a07f8ec5417b80fd
Gael Galaviz <Gael Galaviz@10.176.13.191>
2026-02-19 12:50:58 -0700

    Creacion del dto de cliente

---
+++ src/main/java/Negocio/DTOs/ClienteNombreDTO.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Negocio.DTOs;
+
+/**
+ *
+ * @author Gael Galaviz
+ */
+public class ClienteNombreDTO {
+    
+    private String nombres;
+    private String apellidoPaterno;
+    private String apellidoMaterno;
+
+    public ClienteNombreDTO(String nombres, String apellidoPaterno, String apellidoMaterno) {
+        this.nombres = nombres;
+        this.apellidoPaterno = apellidoPaterno;
+        this.apellidoMaterno = apellidoMaterno;
+    }
+
+    public ClienteNombreDTO() {
+    }
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public String getApellidoPaterno() {
+        return apellidoPaterno;
+    }
+
+    public void setApellidoPaterno(String apellidoPaterno) {
+        this.apellidoPaterno = apellidoPaterno;
+    }
+
+    public String getApellidoMaterno() {
+        return apellidoMaterno;
+    }
+
+    public void setApellidoMaterno(String apellidoMaterno) {
+        this.apellidoMaterno = apellidoMaterno;
+    }
+    
+    
+    
+}
