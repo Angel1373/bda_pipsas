57369bc6852678f57eb221a3bbbcb43265b9b55d
Angel1373 <angel.gaxiolap3@gmail.com>
2026-02-18 20:50:17 -0700

    Primer commit e inicio del proyecto

---
+++ src/main/java/com/mycompany/bda_pipsas/Bda_pipsas.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.bda_pipsas;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Bda_pipsas {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
